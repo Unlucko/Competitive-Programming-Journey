311d184f713ed47fbe45f73ca1bbec0746e7ce97
Unlucko <82614744+Unlucko@users.noreply.github.com>
2025-04-30 08:02:36 -0500

    Update magnets.cpp

--- 800/magnets.cpp
+++ 800/magnets.cpp
@@ -18,8 +18,8 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);  
-    int NCount, NNum=0;
+    	cin.tie(NULL);  
+    	int NCount, NNum=0;
 	std::string SActMagnet="", SPasMagnet="";
 	std::cin >> NCount;
 	while(NCount--){std::cin>>SActMagnet; NNum += (SActMagnet==SPasMagnet)? 0:1; SPasMagnet=SActMagnet;}
