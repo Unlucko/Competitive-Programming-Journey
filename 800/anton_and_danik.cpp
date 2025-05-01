ae80bfdd4ac4afe1b884aeabe2e3a5a3673b28fa
Unlucko <82614744+Unlucko@users.noreply.github.com>
2025-05-01 18:39:02 -0500

    Update anton_and_danik.cpp

--- 800/anton_and_danik.cpp
+++ 800/anton_and_danik.cpp
@@ -1,14 +1,38 @@
+/*
+Anton likes to play chess, and so does his friend Danik.
+
+Once they have played n games in a row. For each game it's known who was the winner — Anton or Danik. None of the games ended with a tie.
+
+Now Anton wonders, who won more games, he or Danik? Help him determine this.
+
+Input
+The first line of the input contains a single integer n (1 ≤ n ≤ 100 000) — the number of games played.
+
+The second line contains a string s, consisting of n uppercase English letters 'A' and 'D' — the outcome of each of the games. The i-th character of the string is equal to 'A' if the Anton won the i-th game and 'D' if Danik won the i-th game.
+
+Output
+If Anton won more games than Danik, print "Anton" (without quotes) in the only line of the output.
+
+If Danik won more games than Anton, print "Danik" (without quotes) in the only line of the output.
+
+If Anton and Danik won the same number of games, print "Friendship" (without quotes).
+*/
+
 #include <bits/stdc++.h> 
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    int nGames, nOutput=0;
-    std::string sGames, sOutput;std::cin>>nGames>>sGames;
-    while(nGames--){
-    	nOutput+=(sGames[nGames]=='A')? 1:-1;
-    }
-    sOutput=(nOutput>0)? "Anton" : (nOutput<0)? "Danik" : "Friendship";
-    std::cout<<sOutput;
+	cin.tie(NULL); 
+	int nGames, nOutput=0;
+	std::string sGames, sOutput;std::cin>>nGames>>sGames;
+	while(nGames--){
+	nOutput+=(sGames[nGames]=='A')? 1:-1;
+	}
+	sOutput=(nOutput>0)? "Anton" : (nOutput<0)? "Danik" : "Friendship";
+	std::cout<<sOutput;
 }
+
+/*
+Just sum to a number the number of times anton appears or substract the ones danik appears and output the corresponding string
+*/
